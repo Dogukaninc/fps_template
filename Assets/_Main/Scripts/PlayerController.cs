7b86041e624db4e11dca3f21a9d52fa5f1ae34bf
Doğukan <d_incirbolen@hotmail.com>
2026-01-29 00:22:44 +0300

    +External assets and models added

--- Assets/_Main/Scripts/PlayerController.cs
+++ Assets/_Main/Scripts/PlayerController.cs
@@ -1,16 +1,79 @@
 using UnityEngine;
+using UnityEngine.InputSystem; // Namespace'i eklemeyi unutma
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private GameInput _input;
+    
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public float mouseSensitivity = 10f;
+
+    public Transform playerCamera;
+    private float _xRotation = 0f;
+
+    private void Awake()
     {
+        _input = new GameInput();
         
+        _input.Player.Fire.performed += ctx => OnLeftClick();
+        _input.Player.AltFire.performed += ctx => OnRightClick();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
+        _input.Player.Enable();
         
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnDisable()
+    {
+        _input.Player.Disable();
+        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleLook();
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 moveInput = _input.Player.Move.ReadValue<Vector2>();
+
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+
+        transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void HandleLook()
+    {
+        // 'Look' action'ından mouse delta verisini oku
+        Vector2 lookInput = _input.Player.Look.ReadValue<Vector2>();
+
+        float mouseX = lookInput.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = lookInput.y * mouseSensitivity * Time.deltaTime;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f); // Boyun kırma sınırı
+
+        playerCamera.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void OnLeftClick()
+    {
+        Debug.Log("Mouse 0 (Sol Tık) algılandı!");
+    }
+
+    private void OnRightClick()
+    {
+        Debug.Log("Mouse 1 (Sağ Tık) algılandı!");
     }
 }
